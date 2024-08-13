31d1ed4cafae0a30456cb66114f469ecf0b91953
yomex96 <abayomirobertonawole2@gmail.com>
2024-08-13 00:52:44 -0700

    okay

--- 0x03-user_authentication_service/main.py
+++ 0x03-user_authentication_service/main.py
@@ -1,15 +1,16 @@
-#!/usr/bin/env python3
-"""
-Main file
-"""
-from auth import Auth
+EMAIL = "guillaume@holberton.io"
+PASSWD = "b4l0u"
+NEW_PASSWD = "t4rt1fl3tt3"
 
-email = 'bob@bob.com'
-password = 'MyPwdOfBob'
-auth = Auth()
 
-auth.register_user(email, password)
+if __name__ == "__main__":
 
-print(auth.create_session(email))
-print(auth.create_session("unknown@email.com"))
-
+    register_user(EMAIL, PASSWD)
+    log_in_wrong_password(EMAIL, NEW_PASSWD)
+    profile_unlogged()
+    session_id = log_in(EMAIL, PASSWD)
+    profile_logged(session_id)
+    log_out(session_id)
+    reset_token = reset_password_token(EMAIL)
+    update_password(EMAIL, reset_token, NEW_PASSWD)
+    log_in(EMAIL, NEW_PASSWD)
